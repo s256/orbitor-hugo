a58b6fdb69fc6d5dfaceb6a81f51714cf5302d57
Sebastian Noe <sebastian.schneider@boxine.de>
2026-02-09 22:05:30 +0100

    add lightbox

--- static/js/script.js
+++ static/js/script.js
@@ -107,7 +107,7 @@
     ],
   });
 
-  $(".portfolio-gallery, .service-gallery").each(function () {
+  $(".portfolio-gallery").each(function () {
     $(this)
       .find(".popup-gallery")
       .magnificPopup({
@@ -118,24 +118,14 @@
       });
   });
 
-  var map;
-
-  function initialize() {
-    var mapOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(50.97797382271958, -114.107718560791),
-      // styles: style_array_here
-    };
-    map = new google.maps.Map(
-      document.getElementById("map-canvas"),
-      mapOptions
-    );
-  }
-
-  var google_map_canvas = $("#map-canvas");
-
-  if (google_map_canvas.length) {
-    google.maps.event.addDomListener(window, "load", initialize);
+  // GLightbox for service galleries
+  if (typeof GLightbox !== "undefined" && document.querySelector(".glightbox")) {
+    GLightbox({
+      selector: ".glightbox",
+      touchNavigation: true,
+      loop: true,
+      closeOnOutsideClick: true,
+    });
   }
 
   // Counter
